83957ab5d2e7f01327f840360ceecc5873e63429
Reinhard Fink <reinhard.fink@tsn.at>
2014-11-03 21:20:59 +0100

    changed path in start.sh

--- tftp/start.sh
+++ tftp/start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-DATAROOT="/data"
 DATAPATH="/var/lib/"
 DATADIR="tftpboot"
 
 GITREPO="http://github.com/edvapp/tftpboot.git"
 
 # clone or pull from repro
-if [ -e ${DATAROOT}${DATAPATH}${DATADIR}/.git ];
+if [ -e ${DATAPATH}${DATADIR}/.git ];
 then
 	echo "pull from " ${GITREPO}
-	cd ${DATAROOT}${DATAPATH}${DATADIR}
+	cd ${DATAPATH}${DATADIR}
 	sudo git pull
 else
 	echo "clone " ${GITREPO}
-	cd ${DATAROOT}${DATAPATH}
+	cd ${DATAPATH}
 	sudo git clone ${GITREPO}
 fi
  
-cd ${DATAROOT}${DATAPATH}${DATADIR}
+cd ${DATAPATH}${DATADIR}
 ./start.sh
